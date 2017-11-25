27f53f9a18fa5fa04a9e641e3ea8621711f1b71c
Tennin Yan <tcccc33@gmail.com>
2017-11-25 21:46:26 +0900

    write swipe in db

--- app/assets/javascripts/jTinder/main.js
+++ app/assets/javascripts/jTinder/main.js
@@ -1,16 +1,37 @@
 /**
  * jTinder initialization
  */
+
+
 $("#tinderslide").jTinder({
+  
 	// dislike callback
     onDislike: function (item) {
 	    // set the status text
-        $('#status').html('Dislike image ' + (item.index()+1));
+      	var uid = document.getElementById("user_uid").value;
+        $.ajax({
+          url: '/update_question?uid=' + uid + '&qid=question'+ (item.index()+1) + '&ans=0',
+          type: 'GET',
+          dataType: 'json',
+          async: true,
+          data: {},
+        }).done(function(data){
+        }).fail(function(data){
+        });
     },
 	// like callback
     onLike: function (item) {
 	    // set the status text
-        $('#status').html('Like image ' + (item.index()+1));
+      	var uid = document.getElementById("user_uid").value;
+        $.ajax({
+          url: '/update_question?uid=' + uid + '&qid=question'+ (item.index()+1) + '&ans=1',
+          type: 'GET',
+          dataType: 'json',
+          async: true,
+          data: {},
+        }).done(function(data){
+        }).fail(function(data){
+        });
     },
 	animationRevertSpeed: 200,
 	animationSpeed: 400,
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
     end
   end
 
+  get '/update_question' => 'users#update_question'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
