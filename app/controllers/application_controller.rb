d3514bb122a38a67b5bac65036912ed4d2d55437
Taku <taku.nakamura0904@gamil.com>
2017-11-26 12:51:34 +0900

    change the top page & login to user's page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  #protect_from_forgery with: :exception
+  before_action :require_login
+
+  private
+
+  def require_login
+    unless session[:user_id]
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to root_path # halts request cycle
+    end
+  end
 end
