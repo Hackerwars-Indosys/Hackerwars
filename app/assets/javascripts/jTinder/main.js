b6c2ce404d9875151f3c29bfda0646932c96040b
Tennin Yan <tcccc33@gmail.com>
2017-11-26 13:32:48 +0900

    changed api & redirect at last

--- app/assets/javascripts/jTinder/main.js
+++ app/assets/javascripts/jTinder/main.js
@@ -8,30 +8,38 @@ $("#tinderslide").jTinder({
 	// dislike callback
     onDislike: function (item) {
 	    // set the status text
-      	var uid = document.getElementById("user_uid").value;
+      	var id = document.getElementById("user_id").value;
         $.ajax({
-          //url: '/update_question?uid=' + uid + '&qid=question'+ (item.index()+1) + '&ans=0',
-          url: '/update_question?uid=1&qid=question'+ (item.index()+1) + '&ans=0',
+          url: '/update_question?uid=' + id + '&qid=question'+ (item.index()+1) + '&ans=0',
+          // url: '/update_question?uid=1&qid=question'+ (item.index()+1) + '&ans=0',
           type: 'GET',
           dataType: 'json',
           async: true,
           data: {},
         }).done(function(data){
+          if (document.getElementById("last").style.display === 'none'){
+            window.location = "result";
+            console.log("redirect!!");
+          }
         }).fail(function(data){
         });
     },
 	// like callback
     onLike: function (item) {
 	    // set the status text
-      	var uid = document.getElementById("user_uid").value;
+      	var id = document.getElementById("user_id").value;
         $.ajax({
-          //url: '/update_question?uid=' + uid + '&qid=question'+ (item.index()+1) + '&ans=1',
-          url: '/update_question?uid=1&qid=question'+ (item.index()+1) + '&ans=1',
+          url: '/update_question?uid=' + id + '&qid=question'+ (item.index()+1) + '&ans=1',
+          // url: '/update_question?uid=1&qid=question'+ (item.index()+1) + '&ans=1',
           type: 'GET',
           dataType: 'json',
           async: true,
           data: {},
         }).done(function(data){
+          if (document.getElementById("last").style.display === 'none'){
+            window.location = "result";
+            console.log("redirect!!");
+          }
         }).fail(function(data){
         });
     },
@@ -39,7 +47,7 @@ $("#tinderslide").jTinder({
 	animationSpeed: 400,
 	threshold: 1,
 	likeSelector: '.like',
-	dislikeSelector: '.dislike'
+	dislikeSelector: '.dislike',
 });
 
 /**
